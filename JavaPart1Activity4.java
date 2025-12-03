5a546d1e872f56920913d8f090f2f9aa6e37a160
joshuatabucanon <joshuatabucanon@gmail.com>
2025-12-03 14:54:46 +0800

    Add files via upload

---
+++ JavaPart1Activity4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class JavaPart1Activity4 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+        	System.out.print("Enter your age: ");
+        	int age = input.nextInt();
+
+        	if (age < 18) {
+            		System.out.println("Minor");
+        	} else if (age >= 18 && age <= 59) {
+            		System.out.println("Adult");
+        	} else {
+            		System.out.println("Senior");
+        	}
+
+        	input.close();
+	}
+}
