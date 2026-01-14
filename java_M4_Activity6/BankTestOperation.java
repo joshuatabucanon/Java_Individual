ed2019d692afd329135b30565f5725f2965e0591
joshuatabucanon <joshuatabucanon@gmail.com>
2026-01-14 15:39:11 +0800

    M4 Activity6

---
+++ java_M4_Activity6/BankTestOperation.java
@@ -0,0 +1,7 @@
+package java_M4_Activity6;
+
+@FunctionalInterface
+public interface BankTestOperation {
+    void execute() throws InvalidAmountException, InsufficientFundsException;
+}
+
---
+++ java_M4_Activity6/InvalidAmountException.java
@@ -0,0 +1,14 @@
+package java_M4_Activity6;
+
+public class InvalidAmountException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidAmountException(String message) {
+        super(message);
+    }
+
+}
