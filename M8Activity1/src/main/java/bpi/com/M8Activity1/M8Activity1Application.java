451e4d94c0ea563067703dc96dafd2de14a24d67
joshuatabucanon <joshuatabucanon@gmail.com>
2026-03-03 11:39:00 +0800

    Add files via upload

---
+++ M8Activity1/src/main/java/bpi/com/M8Activity1/M8Activity1Application.java
@@ -0,0 +1,13 @@
+package bpi.com.M8Activity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M8Activity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M8Activity1Application.class, args);
+	}
+
+}
---
+++ M8Activity1/src/test/java/bpi/com/M8Activity1/M8Activity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bpi.com.M8Activity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M8Activity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
