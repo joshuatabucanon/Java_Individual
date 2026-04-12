9a7512e58b22c583052146295e96dc10b39a8771
joshuatabucanon <joshuatabucanon@gmail.com>
2026-04-12 23:07:41 +0800

    M8_Activity11 / Exercise 8.6
    Adding DTO

---
+++ M8Activity11/src/main/java/com/bpi/m8activity11/M8Activity11Application.java
@@ -0,0 +1,15 @@
+package com.bpi.m8activity11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class M8Activity11Application {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(M8Activity11Application.class, args);
+
+
+    }
+}
