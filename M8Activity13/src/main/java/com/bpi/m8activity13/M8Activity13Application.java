197a115a5e9989b753bdfd4e37e12db3b9eb9b86
joshuatabucanon <joshuatabucanon@gmail.com>
2026-04-12 23:18:52 +0800

    M8_Activity13 / Exercise 8.8
    Adding DTO

---
+++ M8Activity13/src/main/java/com/bpi/m8activity13/M8Activity13Application.java
@@ -0,0 +1,15 @@
+package com.bpi.m8activity13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class M8Activity13Application {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(M8Activity13Application.class, args);
+
+
+    }
+}
---
+++ M8Activity13/src/main/java/com/bpi/m8activity13/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.bpi.m8activity13.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping
+    public String showWelcomeMessage() {
+        return message;
+    }
+}
