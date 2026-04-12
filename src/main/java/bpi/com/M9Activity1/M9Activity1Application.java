3d8a77f939d63fc4891506ef4b83a8eb4fc9967a
joshuatabucanon <joshuatabucanon@gmail.com>
2026-04-12 23:38:56 +0800

    M9_Activity1
    Add Spring Security to Spring Boot

---
+++ src/main/java/bpi/com/M9Activity1/M9Activity1Application.java
@@ -0,0 +1,13 @@
+package bpi.com.M9Activity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M9Activity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M9Activity1Application.class, args);
+	}
+
+}
