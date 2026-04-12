78f74667ea8ae770d76d890f65b06f0f4462056c
joshuatabucanon <joshuatabucanon@gmail.com>
2026-04-12 23:13:24 +0800

    M8_Activity12 / Exercise 8.7
    Profile Setup

---
+++ M8Activity12/src/main/java/com/bpi/m8activity12/M8Activity12Application.java
@@ -0,0 +1,15 @@
+package com.bpi.m8activity12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class M8Activity12Application {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(M8Activity12Application.class, args);
+
+
+    }
+}
