651fd6f89c2abf58646bc03830d29a90d1c2fa88
joshuatabucanon <joshuatabucanon@gmail.com>
2026-04-12 22:59:33 +0800

    M8_Activity8 / Exercise 8.3
    Implementing @PathVariable

---
+++ M8Activity8/src/main/java/com/bpi/M8Activity8/M8Activity8Application.java
@@ -0,0 +1,15 @@
+package com.bpi.M8Activity8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class M8Activity8Application {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(M8Activity8Application.class, args);
+
+
+    }
+}
