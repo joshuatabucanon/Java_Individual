7d9fe674c7f330bda8c06a3d0292535d225321d0
joshuatabucanon <joshuatabucanon@gmail.com>
2026-04-12 22:53:33 +0800

    M8_Activity6 / Exercise 8.1
    Implementing GET

---
+++ M8Activity6/src/main/java/com/bpi/M8Activity6/M8Activity6Application.java
@@ -0,0 +1,15 @@
+package com.bpi.M8Activity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class M8Activity6Application {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(M8Activity6Application.class, args);
+
+
+    }
+}
